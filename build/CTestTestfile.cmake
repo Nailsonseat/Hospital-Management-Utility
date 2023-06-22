d7dcfcb94f375f5363b4325a69e02e95a12727c6
nailsonseat <aadarshv3@gmail.com>
2023-06-22 13:26:01 +0530

    Removed enviornment specfic build files

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nailsonseat/Desktop/C++/Hospital Management Utility
-# Build directory: /home/nailsonseat/Desktop/C++/Hospital Management Utility/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
